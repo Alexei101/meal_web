ee4746af67ff74c48e158d49113a519eb13e43a9
Alexei101 <alexakalexa649@gmail.com>
2024-09-08 18:02:03 +0300

    test

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 
-
+from flask import Flask
 import requests
 
+app = Flask(__name__)
 
-if __name__ == '__main__':
+@app.route('/')
+def get_recipe():
     response = requests.get("https://www.themealdb.com/api/json/v1/1/random.php")
     ans = response.json()
     recipe_instructions = ans['meals'][0]['strInstructions']
     label = ans['meals'][0]['strMeal']
-    print(label)
-    print(recipe_instructions)
+    return f'<h1>{label}</h1><p>{recipe_instructions}</p>'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5080)  # Запуск приложения
